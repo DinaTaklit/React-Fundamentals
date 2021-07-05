5dcde0986705a48c23320608044ce8c44686e66e
DinaTaklit <ez_taklit@esi.dz>
2021-07-05 07:50:03 +0100

    feat: 1.UseState: complex Counter

--- src/component/1.UseState/Counter.js
+++ src/component/1.UseState/Counter.js
@@ -2,11 +2,20 @@ import React, {useState} from 'react'
 
 export default function Counter() {
     const [value, setValue] = useState(0)
+    const [complexValue, setComplexValue] = useState(0)
 
     const reset = () => {
         setValue(0)
     }
-    
+
+    const complexIncrease= () => {
+        setTimeout(() => {
+            //setComplexValue(complexValue +1); // If u click many time still u get same result casue it used the old original value.
+            setComplexValue((prevComplexValue) => {
+                return prevComplexValue +1
+            })
+        }, 1000);
+    }
     return (
         <>
             <section>
@@ -34,7 +43,31 @@ export default function Counter() {
                     Reset
                 </button>
             </section>
+            <section>
+                <h2 style={{margin:"4rem 0"}}> Complex Counter</h2>
+                <h3>{complexValue}</h3>
 
+                <button
+                    className="btn"
+                    onClick={complexIncrease}
+                >
+                    Increase
+                </button>
+
+                <button
+                    className="btn"
+                    onClick={() => setComplexValue(complexValue-1) }
+                >
+                    Decrease
+                </button>
+
+                <button
+                    className="btn"
+                    onClick={() => setComplexValue(0)}
+                >
+                    Reset
+                </button>
+            </section>
         </>
     )
 }
