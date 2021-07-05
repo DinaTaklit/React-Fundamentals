895c9edf80c1dc6aba74dd7820a7743fec2fed31
DinaTaklit <ez_taklit@esi.dz>
2021-07-05 23:35:32 +0100

    4.Form: controlled inputs

---
+++ src/component/4.Forms/ControlledInputs.js
@@ -0,0 +1,59 @@
+import React, {useState} from 'react'
+
+export default function ControlledInputs() {
+
+    const [firstName, setFirstName] = useState("")
+    const [email, setEmail] = useState("")
+    const [people, setPeople] = useState([])
+
+    const handleSubmit = e =>{
+        e.preventDefault()
+        if (firstName && email){
+            const person = {id:new Date().getTime.toString() ,firstName, email}
+            setPeople(prevPeople => [...prevPeople, person])
+        }
+    }
+    return (
+        <>
+            <h2 style={{marginBottom:'2rem'}}>Controlled Inputs</h2>
+            <section>
+                <form className="form" onSubmit={handleSubmit}>
+                    <div className="form-control">
+                        <label htmlFor="firstName"> Name:</label>
+                        <input 
+                            type="text" 
+                            id="firstName"
+                            name="firstName"
+                            placeholder="your name"
+                            value={firstName}
+                            onChange={(e) => setFirstName(e.target.value)}
+                        />
+                    </div>
+                    <div className="form-control">
+                        <label htmlFor="email"> Email:</label>
+                        <input 
+                            type="email" 
+                            id="email"
+                            name="email"
+                            placeholder="your email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <button type="submit"> Add person</button>
+                </form>
+            </section>
+            <section>
+                {people.map(person=> {
+                    const {id, firstName, email} = person
+                    return (
+                        <div className='item' key={id}>
+                            <h3>{firstName}</h3>
+                            <p>{email}</p>
+                        </div>
+                    )
+                })}
+            </section>
+        </>
+    )
+}
--- src/component/App.js
+++ src/component/App.js
@@ -9,12 +9,13 @@ import '../style/App.css';
 // import FetchGithubUsers from './2.UseEffect/FetchGithubUsers'
 // import MultipleReturns from './3.ConditionalRendering/MultipleReturns'
 // import ShortCircuit from './3.ConditionalRendering/ShortCircuit'
-import ShowHide from './3.ConditionalRendering/ShowHide'
+// import ShowHide from './3.ConditionalRendering/ShowHide'
+import ControlledInputs from './4.Forms/ControlledInputs'
 
 const App = () => {
   return (
     <div className="container">
-      <ShowHide/>
+      <ControlledInputs/>
     </div>
   );
 }
