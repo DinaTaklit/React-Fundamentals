9a632678532db06f28c158aa767a1edced9cdd3b
DinaTaklit <ez_taklit@esi.dz>
2021-07-07 21:06:24 +0100

    feat: 12.MemoUseMemoUseCallback: use Memo to prevent Big List from rendering each time count change

--- src/component/12.MemoUseMemoUseCallback/index.js
+++ src/component/12.MemoUseMemoUseCallback/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useFetch } from '../9.CustomHooks/useFetch'
 
 const url = 'https://course-api.com/javascript-store-products'
@@ -6,48 +6,54 @@ const url = 'https://course-api.com/javascript-store-products'
 // every time props or state changes, component re-renders
 
 const Index = () => {
-  const [isLoadig, products]  = useFetch(url)
-  const [count, setCount] = useState(0)
+    const [isLoadig, products]  = useFetch(url)
+    const [count, setCount] = useState(0)
 
-  return (
-    <>
+    return (
+        <>
+            <h1>Count : {count}</h1>
+            <button className='btn' onClick={() => setCount(count + 1)}>
+                click me
+            </button>
+            <div>
+                {isLoadig && 'Loading ...'}
+            </div>
 
-      <h1>Count : {count}</h1>
-      <button className='btn' onClick={() => setCount(count + 1)}>
-        click me
-      </button>
-      <div>
-        {isLoadig && 'Loading ...'}
-      </div>
-
- 
-      <BigList products={products} />
-    </>
+            <BigList products={products} />
+        </>
   )
 }
 
-const BigList = ({ products }) => {
-  return (
-    <section className='products'>
-      {products.map((product) => {
-        return <SingleProduct key={product.id} {...product}></SingleProduct>
-      })}
-    </section>
-  )
-}
+const BigList = React.memo(({ products }) => {
+    useEffect(() => {
+        console.log('Big List')
+
+    })
+    return (
+        <section className='products'>
+        {products.map((product) => {
+            return <SingleProduct key={product.id} {...product}></SingleProduct>
+        })}
+        </section>
+    )
+})
 
 const SingleProduct = ({ fields }) => {
-  let { name, price } = fields
-  price = price / 100
-  const image = fields.image[0].url
+    useEffect(() => {
+        console.log('Single Product ')
+    })
 
-  return (
-    <article className='product'>
-      <img src={image} alt={name} />
-      <h4>{name}</h4>
-      <p>${price}</p>
-    </article>
-  )
+    let { name, price } = fields
+    price = price / 100
+    const image = fields.image[0].url
+
+    return (
+        <article className='product'>
+        <img src={image} alt={name} />
+        <h4>{name}</h4>
+        <p>${price}</p>
+        </article>
+    )
 }
 
 export default Index
