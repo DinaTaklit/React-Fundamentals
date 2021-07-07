758bcdc12b6033128a8edc4adbfe26b260f96a68
DinaTaklit <ez_taklit@esi.dz>
2021-07-07 22:52:32 +0100

    9.CustomHooks: use useCallback to optimize useFetch custom hook

--- src/component/9.CustomHooks/useFetch.js
+++ src/component/9.CustomHooks/useFetch.js
@@ -1,18 +1,22 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useCallback } from "react"
 
 export const useFetch = (url) => {
     const [loading, setLoading] = useState(true)
     const [data, setData] = useState([])
 
-    const getData = async () => {
+    // if we add getData to array of dependencies without using useCallback
+    // it will cause infinite loop
+    // Rrecreate getData if the url change
+
+    const getData = useCallback(async () => {
         const data = await fetch(url).then(response => response.json())
         setData(data)
         setLoading(false)
-    }
+    },[url])
 
     useEffect(() => {
         getData()
-    }, [url])
+    }, [url, getData])
 
     return [loading, data]
 }
