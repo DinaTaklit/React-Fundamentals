2db1b4904dd6010844056626545a91cff240daab
DinaTaklit <ez_taklit@esi.dz>
2021-07-07 17:11:12 +0100

    11.ReactRouter: setup Link  to Person from People component

--- src/component/11.ReactRouter/People.js
+++ src/component/11.ReactRouter/People.js
@@ -1,4 +1,5 @@
 import {useState} from 'react'
+import {Link} from 'react-router-dom'
 import { data } from '../../data';
 const People = () => {
     const [people, setPeople] = useState(data);
@@ -9,6 +10,7 @@ const People = () => {
           return (
             <div key={person.id} className='item'>
               <h4>{person.name}</h4>
+              <Link to={`/persons/${person.id}`}>Learn More</Link>
             </div>
           );
         })}
