a7eca5238dcebc5eb772a630e796237d5aa972fd
DinaTaklit <ez_taklit@esi.dz>
2021-07-07 21:49:51 +0100

    12.MemoUseMemoUseCallback: useCallabck to prevent Big List from rerendering each time we click the button

--- src/component/12.MemoUseMemoUseCallback/index.js
+++ src/component/12.MemoUseMemoUseCallback/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import { useFetch } from '../9.CustomHooks/useFetch'
 
 const url = 'https://course-api.com/javascript-store-products'
@@ -8,6 +8,11 @@ const url = 'https://course-api.com/javascript-store-products'
 const Index = () => {
     const [isLoadig, products]  = useFetch(url)
     const [count, setCount] = useState(0)
+    const [cart, setCart] = useState(0)
+
+    const addToCart = useCallback(() => {
+        setCart(cart +1 )
+    }, [cart])
 
     return (
         <>
@@ -19,12 +24,12 @@ const Index = () => {
                 {isLoadig && 'Loading ...'}
             </div>
 
-            <BigList products={products} />
+            <BigList products={products}  addToCart={addToCart} />
         </>
   )
 }
 
-const BigList = React.memo(({ products }) => {
+const BigList = React.memo(({ products, addToCart }) => {
     useEffect(() => {
         console.log('Big List')
 
@@ -32,13 +37,13 @@ const BigList = React.memo(({ products }) => {
     return (
         <section className='products'>
         {products.map((product) => {
-            return <SingleProduct key={product.id} {...product}></SingleProduct>
+            return <SingleProduct key={product.id} {...product}  addToCart={ addToCart}></SingleProduct>
         })}
         </section>
     )
 })
 
-const SingleProduct = ({ fields }) => {
+const SingleProduct = ({ fields,  addToCart }) => {
     useEffect(() => {
         console.log('Single Product ')
     })
@@ -52,6 +57,7 @@ const SingleProduct = ({ fields }) => {
         <img src={image} alt={name} />
         <h4>{name}</h4>
         <p>${price}</p>
+        <button onClick={addToCart}>add to cart</button>
         </article>
     )
 }
