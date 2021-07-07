0f5480928d8062fe51bdc3afd4663594b4468980
DinaTaklit <ez_taklit@esi.dz>
2021-07-07 14:46:49 +0100

    10.PropTypes: prop types and short circuit

--- src/component/10.PropTypes/Product.js
+++ src/component/10.PropTypes/Product.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import defaultImage from '../../assets/default.jpg'
 
-export default function Product() {
+export default function Product({image, name, price}) {
+    const url = image && image.url
     return (
-        <div>
-            <h3>Product</h3>
+        <div className="product">
+            <img src={url || defaultImage} alt={name || 'Default Name'} />
+            <h3>{name || 'Default Name'}</h3>
+            <p>${price || 3.99}</p>
         </div>
     )
 }
+
+Product.propType = {
+    image: PropTypes.object.isRequired,
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+}
+
+// Product.defaultProps = {
+//     name: 'default name',
+//     price: 3.99,
+//     image: ''
+// }
