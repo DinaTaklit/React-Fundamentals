3fb013581157cba13b356e2ad039cdaa1505c81a
DinaTaklit <ez_taklit@esi.dz>
2021-07-07 17:17:20 +0100

    11.ReactRouter: setup Person component

--- src/component/11.ReactRouter/Person.js
+++ src/component/11.ReactRouter/Person.js
@@ -1,7 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import {Link, useParams} from 'react-router-dom'
+import { data } from '../../data';
+
+
 const Person = () => {
+
+    const [name, setName] = useState('')
+    const {id} = useParams()
+
+    useEffect(() => {
+        const person = data.find(person => person.id === parseInt(id))
+        setName(person.name)
+    }, [])
+
     return (
       <div>
-        <h2>person</h2>
+        <h2>{name}</h2>
+        <Link className="btn" to="/people"> Back to People</Link>
       </div>
     );
 };
