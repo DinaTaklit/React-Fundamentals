c6846a400298d32fcb501b5ea0e564854b763607
DinaTaklit <ez_taklit@esi.dz>
2021-07-07 17:18:53 +0100

    11.ReactRouter: add back to home in Error page

--- src/component/11.ReactRouter/Error.js
+++ src/component/11.ReactRouter/Error.js
@@ -1,7 +1,10 @@
+import {Link} from 'react-router-dom'
+
 export default function Error () {
     return (
       <div>
         <h1>Error Page</h1>
+        <Link to="/" className="btn"> Back to Home</Link>
       </div>
     );
 };
