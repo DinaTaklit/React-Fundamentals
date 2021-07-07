d584fd83d356e211c15f1fb5e3f72e85169cd15f
DinaTaklit <ez_taklit@esi.dz>
2021-07-07 16:46:58 +0100

    11.ReactRouter: setup routes

--- src/component/11.ReactRouter/People.js
+++ src/component/11.ReactRouter/People.js
@@ -1,5 +1,5 @@
 import {useState} from 'react'
-import { data } from '../../../data';
+import { data } from '../../data';
 const People = () => {
     const [people, setPeople] = useState(data);
     return (
--- src/component/11.ReactRouter/index.js
+++ src/component/11.ReactRouter/index.js
@@ -1,9 +1,29 @@
 import React from 'react'
 
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+// pages
+import Home from './Home';
+import About from './About';
+import People from './People';
+import Error from './Error';
+import Person from './Person';
+// navbar
+import Navbar from './Navbar';
+
 export default function ReactRouterSetup() {
     return (
-        <>
+        <Router>
             <h2 style={{marginBottom:"3rem"}}> React Router </h2>
-        </>
+            <Route path="/">
+                <Home/>
+            </Route>
+            <Route path="/about">
+                <About/>
+            </Route>
+            <Route path="/people">
+                <People/>
+            </Route>
+        </Router>
     )
 }
